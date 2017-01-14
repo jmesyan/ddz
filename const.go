a25314de7b4a442a0d3e16abb52ef095cee5511e
Master.G <dearunclexiao@qq.com>
2017-01-14 15:00:19 +0800

    update

--- const.go
+++ const.go
@@ -1,5 +1,19 @@
 package golandlord
 
 const (
-    CardRank3 = 1
+    CardRank3 = 0x01
+    CardRank4 = 0x02
+    CardRank5 = 0x03
+    CardRank6 = 0x04
+    CardRank7 = 0x05
+    CardRank8 = 0x06
+    CardRank9 = 0x07
+    CardRankT = 0x08
+    CardRankJ = 0x09
+    CardRankQ = 0x0A
+    CardRankK = 0x0B
+    CardRankA = 0x0C
+    CardRank2 = 0x0D
+    CardRankr = 0x0E
+    CardRankR = 0x0F
 )
