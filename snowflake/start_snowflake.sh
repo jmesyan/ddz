dff22d026a256b7b99082e611e9bd365f420b101
Master.G <dearunclexiao@qq.com>
2017-02-06 17:27:50 +0900

    update

--- snowflake/start_snowflake.sh
+++ snowflake/start_snowflake.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker run --name snowflake1 -e SERVICE_ID=snowflake1 -e MACHINE_ID=1 -d -P snowflake
+curl -L -X PUT http://127.0.0.1:2379/v2/keys/seqs/userid -d value="0"
+curl -L -X PUT http://127.0.0.1:2379/v2/keys/seqs/snowflake-uuid -d value="0"
+docker run --name snowflake1 -e SERVICE_ID=snowflake1 -e MACHINE_ID=1 -p 40001:40001 -d -P snowflake
