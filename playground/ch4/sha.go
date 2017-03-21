924889da6fa9cdac0da12e03229cc04bccd1e859
mg <dearunclexiao@qq.com>
2017-03-22 01:41:57 +0800

    update

---
+++ playground/ch4/sha.go
@@ -0,0 +1,5 @@
+package ch4
+
+func SHA256Diff(a, b [32]uint8) int {
+	return 0
+}
---
+++ playground/gopl.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/master-g/golandlord/playground/ch1"
+
+func main() {
+	ch1.Serve()
+}
